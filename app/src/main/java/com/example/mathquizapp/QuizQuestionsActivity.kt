a74145b1dbbfa0dcce29d641dc46b35b913d127a
OttoMarina <otto.02@list.ru>
2023-01-31 15:52:24 +0300

    test navigation half completed

--- app/src/main/java/com/example/mathquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/mathquizapp/QuizQuestionsActivity.kt
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
@@ -24,7 +26,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
         val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
         val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
-
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
 
 
         setQuestion()
@@ -33,7 +35,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         tvOptionTwo.setOnClickListener(this)
         tvOptionThree.setOnClickListener(this)
         tvOptionFour.setOnClickListener(this)
-
+        btnSubmit.setOnClickListener(this)
     }
 
     //setting up default options(answer) view
@@ -59,10 +61,16 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun setQuestion() {
-        mCurrentPosition = 1
+        val btnSubmit =  findViewById<Button>(R.id.btn_submit)
         val question = mQuestionsList!![mCurrentPosition - 1]
 
         defaultOptionsView()
+
+        if(mCurrentPosition == mQuestionsList!!.size) {
+            btnSubmit.text = "FINISH"
+        }else{
+            btnSubmit.text = "SUBMIT"
+        }
 
         //take xml markdown elements by id
         val progBar =  findViewById<ProgressBar>(R.id.progressBar)
@@ -102,6 +110,73 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
             R.id.tv_option_four-> {
                 selectedOptionView(tvOptionFour, 4)
             }
+            //implementation of repainting after submit button and next page moving
+            R.id.btn_submit ->{
+                //if no answers were chosen yet
+                if(mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
+
+                    //draw a new question page
+                    when {
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }
+                        else -> {
+                            Toast.makeText(this, "test finished successfully", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }else{
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+                    if(question!!.correctAnswer != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer,R.drawable.correct_option_border_bg)
+                    val btnSubmit = findViewById<Button>(R.id.btn_submit)
+                    if(mCurrentPosition == mQuestionsList!!.size) {
+
+                        btnSubmit.text = "FINISH"
+                    }
+                    else{
+                        btnSubmit.text = "GO TO NEXT QUESTION"
+                    }
+                    //hide chosen answer to render next question page
+                    mSelectedOptionPosition = 0
+
+                }
+            }
+        }
+
+    }
+
+    private fun answerView(answer: Int, drawableView:Int) {
+        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
+        //user clicked at answer, according it`s number we paint it to drawable view
+        when(answer) {
+            1 -> {
+                tvOptionOne.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            2 -> {
+                tvOptionTwo.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            3 -> {
+                tvOptionThree.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            4 -> {
+                tvOptionFour.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+
         }
     }
     private fun selectedOptionView(tv: TextView, selectedOptionNum:Int) {
