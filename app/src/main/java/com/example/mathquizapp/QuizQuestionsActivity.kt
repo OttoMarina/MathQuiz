a7547c72555d7e332de895c72f9b28f520ff846b
OttoMarina <otto.02@list.ru>
2023-01-31 15:14:27 +0300

    answer selection added and reworked questions arraylist

--- app/src/main/java/com/example/mathquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/mathquizapp/QuizQuestionsActivity.kt
@@ -1,20 +1,69 @@
 package com.example.mathquizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    private var mCurrentPosition:Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions Size", "${questionsList.size}")
-        val currentPosition = 1
-        val question:Question? = questionsList[currentPosition - 1]
+        mQuestionsList = Constants.getQuestions()
+
+        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
+
+
+
+        setQuestion()
+
+        tvOptionOne.setOnClickListener(this)
+        tvOptionTwo.setOnClickListener(this)
+        tvOptionThree.setOnClickListener(this)
+        tvOptionFour.setOnClickListener(this)
+
+    }
+
+    //setting up default options(answer) view
+    private fun defaultOptionsView() {
+        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
+        val options = ArrayList<TextView>()
+        options.add(0, tvOptionOne)
+        options.add(1, tvOptionTwo)
+        options.add(2, tvOptionThree)
+        options.add(3, tvOptionFour)
+        //setting up default colors in quiz UI
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    private fun setQuestion() {
+        mCurrentPosition = 1
+        val question = mQuestionsList!![mCurrentPosition - 1]
+
+        defaultOptionsView()
+
         //take xml markdown elements by id
         val progBar =  findViewById<ProgressBar>(R.id.progressBar)
         val tvProgress = findViewById<TextView>(R.id.tv_progress)
@@ -25,8 +74,8 @@ class QuizQuestionsActivity : AppCompatActivity() {
         val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
         val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
 
-        progBar.progress = currentPosition
-        tvProgress.text = "$currentPosition" + "/" + progBar.max
+        progBar.progress = mCurrentPosition
+        tvProgress.text = "$mCurrentPosition" + "/" + progBar.max
         tvQuestion.text = question!!.question
         ivImage.setImageResource(question.image)
         tvOptionOne.text = question.optionOne
@@ -34,4 +83,39 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionThree.text = question.optionThree
         tvOptionFour.text = question.optionFour
     }
+
+    override fun onClick(v: View?) {
+        val tvOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        val tvOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val tvOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        val tvOptionFour = findViewById<TextView>(R.id.tv_option_four)
+        when(v?.id) {
+            R.id.tv_option_one-> {
+                selectedOptionView(tvOptionOne, 1)
+            }
+            R.id.tv_option_two-> {
+                selectedOptionView(tvOptionTwo, 2)
+            }
+            R.id.tv_option_three-> {
+                selectedOptionView(tvOptionThree, 3)
+            }
+            R.id.tv_option_four-> {
+                selectedOptionView(tvOptionFour, 4)
+            }
+        }
+    }
+    private fun selectedOptionView(tv: TextView, selectedOptionNum:Int) {
+        //set another fields by default
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        //repaint selected answer and make it`s text bold
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.default_option_border_bg
+        )
+
+    }
+
 }
